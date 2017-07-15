59082b55a9baf94efa920b696dab9e2fcb14c0c9
Dong-jun Lim <ciqete@gmail.com>
2017-07-16 00:05:39 +0900

    Make the general structure of manipulator

--- clitl.hpp
+++ clitl.hpp
@@ -4,8 +4,17 @@
 #include <cstdio>
 #include <iosfwd>
 #include <locale>
+#include <utility>
 
 namespace clitl {
+	/* Basic definitions and classes */
+	typedef int scord_t;
+
+	class parametric_manipulator {
+		// Empty
+	};
+
+	/* Output buffer */
 	template <typename charT, typename traits = std::char_traits<charT> >
 	class basic_outbuf : public std::basic_streambuf<charT, traits> {
 	protected:
@@ -22,19 +31,43 @@ namespace clitl {
 	typedef basic_outbuf<char> outbuf;
 	typedef basic_outbuf<wchar_t> woutbuf;
 
+	/* Output stream */
 	template <typename charT, typename traits = std::char_traits<charT> >
 	class basic_ostream : public std::basic_ostream<charT, traits> {
 	public:
 		explicit basic_ostream(basic_outbuf<charT, traits>* sb)
 			: std::basic_ostream<charT, traits>(sb)
 		{
-			// empty
+			// Empty
 		}
 	};
 
 	typedef basic_ostream<char> ostream;
 	typedef basic_ostream<wchar_t> wostream;
 
+	class moveto : public parametric_manipulator {
+		std::pair<scord_t, scord_t> pr;
+	public:
+		explicit moveto(std::pair<scord_t, scord_t> pr) : pr(pr)
+		{
+			// Empty
+		}
+
+		template <typename charT, typename traits = std::char_traits<charT> >
+		friend std::basic_ostream<charT, traits>&
+			operator<<(std::basic_ostream<charT, traits>& os, const moveto& mvto);
+	};
+
+	template <typename charT, typename traits>
+	std::basic_ostream<charT, traits>&
+		operator<<(std::basic_ostream<charT, traits>& os, const moveto& mvto)
+	{
+		return os;
+	}
+
+	/* Input buffer */
+
+	/* Input stream */
 	template <typename charT, typename traits = std::char_traits<charT> >
 	class basic_istream : public std::basic_istream<charT, traits> {
 		
