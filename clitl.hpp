78587097c6d9ef94b23ce3a37b4976566d3f1a89
Dong-jun Lim <ciqete@gmail.com>
2017-07-16 23:05:09 +0900

    Untabify and add manipulators and rect

--- clitl.hpp
+++ clitl.hpp
@@ -7,74 +7,78 @@
 #include <utility>
 
 namespace clitl {
-	/* Basic definitions and classes */
-	typedef int scord_t;
+    /* Basic definitions and classes */
+    typedef int coord_t;
 
-	class parametric_manipulator {
-		// Empty
-	};
+    template <typename T>
+    class rect {
+        std::pair<T, T> origin;
+        std::pair<T, T> endpoint;
+    };
 
-	/* Output buffer */
-	template <typename charT, typename traits = std::char_traits<charT> >
-	class basic_outbuf : public std::basic_streambuf<charT, traits> {
-	protected:
-		virtual typename traits::int_type
-			overflow(typename traits::int_type c)
-		{
-			if (std::putchar(c) == EOF) {
-				return traits::eof();
-			}
-			return traits::not_eof(c);
-		}
-	};
+    /* Output buffer */
+    template <typename charT, typename traits = std::char_traits<charT> >
+    class basic_outbuf : public std::basic_streambuf<charT, traits> {
+    protected:
+        virtual typename traits::int_type
+            overflow(typename traits::int_type c)
+        {
+            if (std::putchar(c) == EOF) {
+                return traits::eof();
+            }
+            return traits::not_eof(c);
+        }
+    };
 
-	typedef basic_outbuf<char> outbuf;
-	typedef basic_outbuf<wchar_t> woutbuf;
+    typedef basic_outbuf<char> outbuf;
+    typedef basic_outbuf<wchar_t> woutbuf;
 
-	/* Output stream */
-	template <typename charT, typename traits = std::char_traits<charT> >
-	class basic_ostream : public std::basic_ostream<charT, traits> {
-	public:
-		explicit basic_ostream(basic_outbuf<charT, traits>* sb)
-			: std::basic_ostream<charT, traits>(sb)
-		{
-			// Empty
-		}
-	};
+    /* Output stream */
+    template <typename charT, typename traits = std::char_traits<charT> >
+    class basic_ostream : public std::basic_ostream<charT, traits> {
+    public:
+        explicit basic_ostream(basic_outbuf<charT, traits>* sb)
+            : std::basic_ostream<charT, traits>(sb)
+        {
+            // Empty
+        }
 
-	typedef basic_ostream<char> ostream;
-	typedef basic_ostream<wchar_t> wostream;
+        basic_ostream<charT, traits>& moveto(const std::pair<coord_t, coord_t>& cord)
+        {
+            return *this;
+        }
 
-	class moveto : public parametric_manipulator {
-		std::pair<scord_t, scord_t> pr;
-	public:
-		explicit moveto(std::pair<scord_t, scord_t> pr) : pr(pr)
-		{
-			// Empty
-		}
+        std::pair<coord_t, coord_t> ssize()
+        {
+            return std::pair<coord_t, coord_t>(0, 0);
+        }
+    };
 
-		template <typename charT, typename traits = std::char_traits<charT> >
-		friend std::basic_ostream<charT, traits>&
-			operator<<(std::basic_ostream<charT, traits>& os, const moveto& mvto);
-	};
+    template <typename charT, typename traits = std::char_traits<charT> >
+    basic_ostream<charT, traits> clear(const basic_ostream<charT, traits>& os)
+    {
 
-	template <typename charT, typename traits>
-	std::basic_ostream<charT, traits>&
-		operator<<(std::basic_ostream<charT, traits>& os, const moveto& mvto)
-	{
-		return os;
-	}
+    }
 
-	/* Input buffer */
+    template <typename charT, typename traits = std::char_traits<charT> >
+    basic_ostream<charT, traits> refresh(const basic_ostream<charT, traits>& os)
+    {
 
-	/* Input stream */
-	template <typename charT, typename traits = std::char_traits<charT> >
-	class basic_istream : public std::basic_istream<charT, traits> {
-		
-	};
+    }
 
-	typedef basic_istream<char> istream;
-	typedef basic_istream<wchar_t> wistream;
+    typedef basic_ostream<char> ostream;
+    typedef basic_ostream<wchar_t> wostream;
+
+    /* Input buffer */
+
+    /* Input stream */
+    template <typename charT, typename traits = std::char_traits<charT> >
+    class basic_istream : public std::basic_istream<charT, traits> {
+        
+    };
+
+    typedef basic_istream<char> istream;
+    typedef basic_istream<wchar_t> wistream;
 }
 
 #endif
